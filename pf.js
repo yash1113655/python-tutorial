e0c92afa16d42c546cbe0662860bf47856721b94
yash1113655 <107207954+yash1113655@users.noreply.github.com>
2022-06-17 12:09:26 +0530

    Update pf.js

--- pf.js
+++ pf.js
@@ -28,20 +28,5 @@ function fixLines() {
       var lines = container[i].innerHTML.split("<br>").length;
       container[i].style.backgroundSize = "100% " + (height / (lines - 1) * 2) + "px";
   }
-  let noOfCharac = 150;
-  let description = document.querySelectorAll(".description");
-  description.forEach(description => {
-    if(description.textDescription.length < noOfCharac){
-      description.nextElementSibling.style.display =
-        "none';
-    }
-    else{
-      let displayText = description.textDescription.slice
-      (0,noOfCharac);
-      let moreText = description.textDescription.slice
-      (noOfCharac);
-      description.innerHTML = '${displayText}<span class="dots>...</span>
-      <span class ="hide more">${moreText}</span> ;
-    }
-});
+  
 }
