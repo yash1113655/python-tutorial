8f2724d36edee3e9b6184d20c1e2f209c672bf7f
yash1113655 <107207954+yash1113655@users.noreply.github.com>
2022-06-17 09:24:00 +0530

    Update pf.js

--- pf.js
+++ pf.js
@@ -34,4 +34,36 @@ function fixLines() {
   readMoreBtn.addEventListener('click',(e)=>{
     description.classList.toggle('show-more');
   })
+  let noOfCharac = 150;
+  let description = document.querySelectorAll(".description");
+  description.forEach(description => {
+    if(description.textDescription.length < noOfCharac){
+      description.nextElementSibling.style.display =
+        "none';
+    }
+    else{
+      let displayText = description.textDescription.slice
+      (0,noOfCharac);
+      let moreText = description.textDescription.slice
+      (noOfCharac);
+      description.innerHTML = '${displayText}<span class="dots>...</span>
+      <span class ="hide more">${moreText}</span> ;
+    }
+});
+  
+  
+function readMore(btn){
+  let post = btn.parentElement;
+  post.querySelector(".dots").classList.toggle
+  ("hide");
+  post.querySelector(".more").classList.toggle
+  ("hide");
+  btn.textDescription == "Read More" ? btn.textDescription = "Read Less" : btn.textDescription = "Read More";
+
+}
+  
+
+    
+    
+      
 }
